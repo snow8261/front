bf74fd942eefe8007b412671ce486d3570421f03
TongjieLi <snow8261@gmail.com>
2015-01-15 12:18:05 +0800

    w3school example

---
+++ w3schools/Script.js
@@ -0,0 +1,9 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='Index.html'>Home</a></li>" +
+"<li><a href='Customers.html'>Data</a></li>" +
+"<li><a href='About.html'>About</a></li>" +
+"</ul>";
