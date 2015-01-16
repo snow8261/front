49bd0a0618440e13dfb042f3d2a166127e78b81a
TongjieLi <snow8261@gmail.com>
2015-01-16 16:48:58 +0800

    add more examples

--- w3schools/Script.js
+++ w3schools/Script.js
@@ -4,6 +4,9 @@ document.getElementById("foot01").innerHTML =
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='Index.html'>Home</a></li>" +
-"<li><a href='Customers.html'>Data</a></li>" +
+"<li><a href='Customers-orignal.html'>Customers_orignal</a></li>" +
+"<li><a href='Customers-Ajax.html'>Customers-Ajax</a></li>" +
+"<li><a href='Customers-Ajax-json.html'>Customers-Ajax-json</a></li>" +
+"<li><a href='Customers-AngularJs.html'>Customers-AngularJs</a></li>" +
 "<li><a href='About.html'>About</a></li>" +
 "</ul>";
